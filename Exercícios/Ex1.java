0ca568346fe1cb6cd45d27aa9090bbd6f4b45f48
Ana <contato.anabeatrizoliver@gmail.com>
2022-04-08 18:03:02 -0300

    lista dos exercicios

--- Exercícios/Ex1.java
+++ Exercícios/Ex1.java
@@ -1,5 +1,38 @@
-package java;
+package GFT;
 
+import java.util.Arrays;
+import java.util.Scanner;
+/**
+ * 
+ * @author ana
+ *
+ */
 public class Ex1 {
-
+ // Elabora um sistema que leia três valores (A, B, C) e 
+ //mostre-os na ordem lida. Em seguida, 
+ //	mostre-os em ordem crescente e decrescente..
+	
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		double[] valores = new double[2];	
+		
+		System.out.println("Digite um número: ");
+		valores[0] = entrada.nextDouble();
+		
+		System.out.println("Digite outro número: ");
+		valores[1] = entrada.nextDouble();
+		
+		System.out.println("Digite mais um número: ");
+		valores[2] = entrada.nextDouble();
+		
+		Arrays.sort(valores); 
+		System.out.println( "Elementos em ordem crescente: " 
+		+Arrays.toString(valores); 
+		
+	//	Arrays.sort(valores); 
+	//System.out.println( "Elementos do array em ordem decrescente: " 
+	//	+Arrays.reverse(valores)); 
+	}
 }
---
+++ Exercícios/Ex2.java
@@ -0,0 +1,26 @@
+package GFT;
+
+import java.util.Scanner;
+
+public class Ex2 {
+	
+	// programa que leia um conjunto de dados contendo o número da
+	//matrícula, as três notas e a frequência (número de aulas frequentadas) de
+	// cinco alunos. Calcule e mostre: 
+	
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Informe o número da matrícula");
+		double matrícula = entrada.nextDouble();
+		
+		System.out.println("Digite suas 3 notas: ");
+		double[] notasAlunoA = new double[3];
+		
+		System.out.println("Digite sua frequência: ");
+		double frequencia = entrada.nextDouble();
+				
+		// não consegui terminar, fiquei focada no ex1 :(	
+	}
+}
